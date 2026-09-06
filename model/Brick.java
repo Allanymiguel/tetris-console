1f60349fe6df6e59871d60c3ac30c2034d7397f8
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-09-06 18:44:01 -0300

    Add block in 2x2 array

---
+++ model/Brick.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Arrays;
+
+public class Brick {
+
+    final int LINHAS = 2;
+    final int COLUNAS = 2;
+    
+    char[][] block = {
+            {'A','A'}, 
+            {'A','A'}
+        };
+
+}
