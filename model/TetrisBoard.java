7b931698585a1152af20c5a41fd232b926641cea
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-09-07 10:04:30 -0300

    Start tetris board config for console

--- model/TetrisBoard.java
+++ model/TetrisBoard.java
@@ -4,6 +4,6 @@ public class TetrisBoard {
 
     private static final int ROWS = 20;
     private static final int COLUMNS = 10;
-    private static final int TITLE_SIZE = 30;
+    char[][] grid = new char[ROWS][COLUMNS];
     
 }
