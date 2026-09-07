f9ae9337d3b0524f52a2e553eb4c7f61fcb8a531
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-09-07 10:00:50 -0300

    Add tetris board configuration

---
+++ model/TetrisBoard.java
@@ -0,0 +1,9 @@
+package model;
+
+public class TetrisBoard {
+
+    private static final int ROWS = 20;
+    private static final int COLUMNS = 10;
+    private static final int TITLE_SIZE = 30;
+    
+}
