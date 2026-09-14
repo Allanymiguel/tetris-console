8ba66d18987ace4109049696847668e9ec9a890b
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-09-14 00:51:44 -0300

    Add Main testes

--- app/Main.java
+++ app/Main.java
@@ -3,12 +3,16 @@ package app;
 import java.util.Map;
 
 import model.Shapes;
+import model.TetrisBoard;
 
 public class Main {
     public static void main(String[] args) {
         Shapes s = new Shapes();
 
         Map map = s.getShapes();
-        s.printShape(map, 1);
+        s.printShape(1);
+
+        TetrisBoard tb = new TetrisBoard();
+        tb.printBoard();
     }
 }
