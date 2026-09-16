5cbea1ab18f8986800c89c18c9bbab4afb21d6ea
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-09-15 22:00:23 -0300

    Add Secondary Main method gitignored

--- model/TetrisBoard.java
+++ model/TetrisBoard.java
@@ -1,10 +1,38 @@
 package model;
 
+import java.util.Map;
+
 public class TetrisBoard {
+
+    Shapes s = new Shapes();
+    Map<Integer, int[][]> shapes = s.getMap();
 
     private static final int ROWS = 20;
     private static final int COLUMNS = 10;
     private int[][] grid = new int[ROWS][COLUMNS];
+
+
+
+    public void putPiece(int idPiece) {
+        int[][] piece = s.getPiece(idPiece);
+        if(piece == null) {
+            System.out.println("Piece does not exist.");
+            return;
+        }
+
+        int height = piece.length;
+        int width = piece[0].length;
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if(i < height) {
+                    grid[i][j] = piece
+                }
+            }
+        }
+
+    }
+
 
     public void reset() {
         for(int i = 0; i < ROWS; i++) {
