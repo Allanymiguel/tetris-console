6fe1e07f9bad5b876c2e746c4f7d70555fb39e7d
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-09-16 23:27:52 -0300

    Add Game Controller

---
+++ controller/Game.java
@@ -0,0 +1,45 @@
+package controller;
+
+import model.Shapes;
+import model.TetrisBoard;
+
+public class Game {
+
+    private TetrisBoard board;
+    private Shapes shapes;
+
+    private int currentPieceId;
+    private int currentRow;
+    private int currentCol;
+
+    private boolean gameOver;
+
+    public Game() {
+    }
+
+    public void start() {
+    }
+
+    private void spawnPiece() {
+    }
+
+    public void moveDown() {
+    }
+
+    public void moveLeft() {
+    }
+
+    public void moveRight() {
+    }
+
+    public void rotate() {
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    private void render() {
+    }
+
+}
